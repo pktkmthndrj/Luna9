aba7668c98e6104d5c8c6b85fd99e9ae2ae67889
pktkmthndrj <145834852+pktkmthndrj@users.noreply.github.com>
2023-12-10 17:37:31 +0300

    Create krpc.py

---
+++ krpc.py
@@ -0,0 +1,32 @@
+import krpc
+import matplotlib.pyplot as plt
+
+# Подключение к серверу kRPC
+conn = krpc.connect()
+
+# Получение объекта космического корабля
+vessel = conn.space_center.active_vessel
+
+# Создание массивов для данных о времени и скорости
+time_values = []
+speed_values = []
+
+# Получение скорости корабля на протяжении полета
+while True:
+    time = conn.space_center.ut
+    speed = vessel.flight(vessel.orbit.body.reference_frame).speed
+    time_values.append(time)
+    speed_values.append(speed)
+    print("Время: {}, Скорость корабля: {} м/с".format(time, speed))
+
+    # Проверка условия завершения сбора данных 
+    altitude = vessel.flight().surface_altitude
+    if altitude > 130000:  # Условие окончания программы 
+        break
+
+# Построение графика скорости от времени
+plt.plot(time_values, speed_values)
+plt.title('Зависимость скорости от времени')
+plt.xlabel('Время, s')
+plt.ylabel('Скорость, m/s')
+plt.show()
